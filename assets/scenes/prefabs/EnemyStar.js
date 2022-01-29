d202dc29f1b7b506cd55afe65a1633aee2a2e273
pattodiablo <pattodiablo@gmail.com>
2022-01-29 16:52:51 -0500

    cambiosenemy

--- assets/scenes/prefabs/EnemyStar.js
+++ assets/scenes/prefabs/EnemyStar.js
@@ -8,15 +8,42 @@ class EnemyStar extends Phaser.GameObjects.Sprite {
 	constructor(scene, x, y, texture, frame) {
 		super(scene, x ?? 0, y ?? 0, texture || "enemy1", frame);
 
+		// this (components)
+		new Physics(this);
+
 		/* START-USER-CTR-CODE */
-		// Write your code here.
+		this.createEvent = this.scene.events.once(Phaser.Scenes.Events.UPDATE, this.create, this);
+		this.scene.events.on("update", () => this.update());
+
+		this.sprites = [];
 		/* END-USER-CTR-CODE */
 	}
 
 	/* START-USER-CODE */
 
-	// Write your code here.
+	
 
+	create(){
+		this.image = this.scene.add.sprite(this.x, this.y, 'inkDot');
+		this.image.setBlendMode(Phaser.BlendModes.ADD);
+		this.enemyShadow=this.scene.add.sprite(this.x, this.y, 'enemy1').setBlendMode(Phaser.BlendModes.OVERLAY);
+		this.sprites.push(this.enemyShadow)
+	}
+
+
+	update ()
+    {
+       	this.angle++;
+		   this.sprites.forEach(enemyShadow => {
+			   enemyShadow.angle--;
+			   enemyShadow.x=this.x;
+		   });
+
+		   this.x--;
+	
+		
+	
+    }
 	/* END-USER-CODE */
 }
 
