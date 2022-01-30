e8585144b815b2eab4130f08d9714ea8ec14f0a4
okv94 <57909521+okv94@users.noreply.github.com>
2022-01-30 16:54:55 -0500

    Update Player.js

--- assets/scenes/Player.js
+++ assets/scenes/Player.js
@@ -119,7 +119,7 @@ class Player extends Phaser.GameObjects.Sprite {
 			this.isWalking=true;
 
 		
-			this.scene.GGJ2022_walk01.play();
+			//this.scene.GGJ2022_walk01.play();
 			//this.scene.GGJ2022_walk02.play();
 
 		}else if (this.cursors.left.isDown) {
@@ -128,7 +128,7 @@ class Player extends Phaser.GameObjects.Sprite {
 			this.body.velocity.x=-this.velocityPlayer;
 			this.isWalking=true;
 	
-			this.scene.GGJ2022_walk01.play();
+			//this.scene.GGJ2022_walk01.play();
 			//this.scene.GGJ2022_walk02.play();
 
 		}else{
